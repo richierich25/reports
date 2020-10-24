d33fab55e87655ddd47e9e789b8af08d2095cfae
richierich25 <richierich25@gmail.com>
2020-10-25 00:13:59 +0530

    added complete excel-export examples

--- src/app/kendo-ui/excel-export/excel-export.component.ts
+++ src/app/kendo-ui/excel-export/excel-export.component.ts
@@ -1,29 +1,139 @@
 import { Component, OnInit } from '@angular/core';
-import { aggregateBy, process } from '@progress/kendo-data-query';
+import { aggregateBy, process, orderBy, filterBy, groupBy  } from '@progress/kendo-data-query';
 import { products } from '../products';
 
 @Component({
   selector: 'app-excel-export',
   templateUrl: './excel-export.component.html',
-  styleUrls: ['./excel-export.component.scss']
+  styleUrls: ['./excel-export.component.scss'],
 })
 export class ExcelExportComponent implements OnInit {
-  public aggregates: any[] = [{field: 'UnitPrice', aggregate: 'sum'}];
+  constructor() {}
 
-  public group: any[] = [{
-      field: 'Discontinued',
-      aggregates: this.aggregates
-  }];
+  // Original Report
+  // Report1 with Hidden State
+  // Report2 with Locked
+  public originalData: any[] = products;
+  public fields: string[] = Object.keys(this.originalData[0]);
 
-  public data: any[] = process(products, {
-      group: this.group
+  // Report3 with Group Header
+  public group3: any[] = [
+    {
+      field: 'Category.CategoryName',
+    },
+  ];
+
+  public data3: any[] = process(this.originalData, {
+    group: this.group3,
   }).data;
 
-  public total: any = aggregateBy(products, this.aggregates);
+  // Report4 with COlumn Header
+  // Report5 with Group Footer
+  public aggregates4: any[] = [{ field: 'UnitPrice', aggregate: 'sum' }];
 
-  constructor() { }
+  public group4: any[] = [
+    {
+      field: 'Category.CategoryName',
+      aggregates: this.aggregates4,
+    },
+  ];
 
-  ngOnInit(): void {
+  public data4: any[] = process(this.originalData, {
+    group: this.group4,
+  }).data;
+
+  // Report6 with Footer
+  // Report7 with Multi Column Header
+  public aggregates6: any[] = [{ field: 'UnitPrice', aggregate: 'sum' }];
+
+  public total6: any = aggregateBy(this.originalData, this.aggregates6);
+
+  // ---------- Cell Options -----------
+  // Report8 with Padding Cells
+  public headerPaddingCells: any = {
+    background: '#ff66ff', // pink
+  };
+  public paddingCells: any = {
+    background: '#ff0000', // red
+  };
+  public headerCells: any = {
+    background: '#66ff66', // green
+    textAlign: 'center',
+  };
+  public cells: any = {
+    background: '#6699ff', // blue
+    textAlign: 'center',
+  };
+
+  // ----------Process Helpers ----------
+  // Combined Process
+  public result = process(this.originalData, {
+    group: [
+      {
+        field: 'Category.CategoryName',
+        aggregates: [
+          { aggregate: 'sum', field: 'UnitPrice' },
+          { aggregate: 'sum', field: 'UnitsInStock' },
+        ],
+      },
+    ],
+    sort: [{ field: 'ProductName', dir: 'desc' }],
+    filter: {
+      logic: 'or',
+      filters: [
+        { field: 'Discontinued', operator: 'eq', value: false },
+        { field: 'UnitPrice', operator: 'lt', value: 22 },
+      ],
+    },
+  });
+
+  // Sorting
+  public resultSort = orderBy(this.originalData, [{ field: 'UnitPrice', dir: 'desc' }]);
+
+  // Grouping
+  public resultGroup = groupBy(this.originalData, [{ field: 'category' }, { field: 'subcategory', dir: 'asc' }]);
+
+  // Filtering
+  public resultFilter = filterBy(this.originalData, {
+    logic: 'and',
+    filters: [
+        { field: 'name', operator: 'startswith', value: 'p', ignoreCase: true },
+        { field: 'subcategory', operator: 'eq', value: 'Meat' },
+    ]
+});
+
+  // Aggregating
+  public resultAgg = aggregateBy(this.originalData, [
+    { field: 'productName', aggregate: 'count' },
+    { field: 'unitPrice', aggregate: 'sum' },
+    { field: 'unitPrice', aggregate: 'average' },
+    { field: 'unitsInStock', aggregate: 'min' },
+    { field: 'unitsInStock', aggregate: 'max' },
+]);
+
+  logger(flag: string): void {
+    switch (flag) {
+      case 'combined':
+        console.log(this.result);
+        // console.log(JSON.stringify(this.result)); // to stringify the JSON
+        // console.log(JSON.stringify(this.result, null, 2)); // to provide 2 spaces of indentation
+        break;
+      case 'sorting':
+        console.log(this.resultSort);
+        break;
+      case 'grouping':
+        console.log(this.resultGroup);
+        break;
+      case 'filtering':
+        console.log(this.resultFilter);
+        break;
+      case 'aggregating':
+        console.log(this.resultAgg);
+        break;
+      default:
+        console.log('No option selected');
+    }
   }
 
+  ngOnInit(): void {}
 }
